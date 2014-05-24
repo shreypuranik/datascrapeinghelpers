65cac17ec27b38b618c6497e73cfbbb6def1265e
shreypuranik <code@shreypuranik.co.uk>
2014-05-24 23:01:01 +0100

    Adding in basic XML retrieval method.

---
+++ functions.getXMLFromHTML.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Get resultant XML from the HTML on a page
+ * V 1.0 of this method uses file_get_contents
+ * I'll add in 'flick' methods to use other
+ * HTML retrieval methods when I get the chance
+ */
+
+function getXMLFromHTML($link){
+
+    for($i=0;$i<10;$i++){
+        $html = file_get_contents($link);
+        if ($html) break;
+    }
+    if ($html){
+        $doc = new DomDocument();
+        @$doc->loadHTML($html);
+        $xml = simplexml_import_dom($doc);
+
+        if ($xml) return $xml;
+    }
+}
